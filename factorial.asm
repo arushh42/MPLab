7a7c811c96aceda0bb798e9273cb46a03ebec644
arushh42 <117122136+arushh42@users.noreply.github.com>
2023-05-01 23:30:44 +0530

    Update and rename factorial.exe to factorial.asm

--- factorial.asm
+++ factorial.asm
@@ -1,11 +1,15 @@
-.model small .data
-num dw 05h .code
-MOV ax, @data ; initialize the data segment MOV ds, ax
+.model small 
+.data
+num dw 05h 
+.code
+MOV ax, @data ; initialize the data segment 
+MOV ds, ax
 MOV ax, 01 ; initialize ax = 1
 MOV bx, num ; load the number in cx
 CALL fact ; call procedure
 MOV di, ax ; store lsb of result in di
-MOV bp, 2 ; initialize count for no of times display is called MOV bx, dx ; store msb of result in reg bx
+MOV bp, 2 ; initialize count for no of times display is called 
+MOV bx, dx ; store msb of result in reg bx
 MOV bx, di ; store lsb of result in bx
 DEC bp ; decrement bp
 MOV ah,4ch
@@ -19,4 +23,6 @@ CMP bx,01 ;multiply bx=1
 JNE l12
 RET
 l11:MOV ax,01 ;initialize ax=1
-RET ;return to called program fact ENDP ;end procedure END ;end program
+RET ;return to called program fact 
+ENDP ;end procedure 
+END ;end program
